36a81dd72385be5d58a12fd1741f62aa0080e3d7
GitHubmaster420 <joonavjs7@hotmail.fi>
2020-10-30 17:10:53 +0000

    Top Down Shooter Joona Pohjonen

---
+++ TopDownShooter_Pohjonen_Joona/Assets/Scripts/DestroyOutOfBorders.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyOutOfBorders : MonoBehaviour
+{
+    private float topBound = 30;
+    private float sideBound = 20;
+    private float bottomBound = -0;
+    public GameObject GameOverScreen;
+    private Vector3 GameOverPos = new Vector3(0, 4, 15);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < bottomBound)
+        {
+            // If enemy gets past player
+            Debug.Log("Game Over!");
+            // Load GameOverScreen on top
+            Instantiate(GameOverScreen, GameOverPos, GameOverScreen.transform.rotation);
+            Destroy(gameObject);
+        }
+        if (transform.position.x * transform.position.x > sideBound * sideBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ TopDownShooter_Pohjonen_Joona/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10;
+    public float horizontalInput;
+    public float bordersX = 20;
+
+
+    public GameObject projectilePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Move sideways
+        transform.Translate(Vector3.right * Time.deltaTime * Speed * horizontalInput);
+        
+        if(transform.position.x > bordersX)
+        {
+            transform.position = new Vector3(bordersX, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < -bordersX)
+        {
+            transform.position = new Vector3(-bordersX, transform.position.y, transform.position.z);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Shoot projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
